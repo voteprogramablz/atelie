cf6ff821a1ee47fd6ca907c4b0a1635a7b1cdaaf
Andre Melo <andrermelo1@hotmail.com>
2024-02-07 23:47:46 -0300

    infra meio caminho andado e começando migrations

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('/', function (Request $request) {
+    // $usuario = new Usuario();
+    // $usuario->nome = 'Andre';
+    // $usuario->save();
+
+    $users = DB::table('tipo_usuario')->get();
+
+    return json_encode(['msg' => $users]);
+});
